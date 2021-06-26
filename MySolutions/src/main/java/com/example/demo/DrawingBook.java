2aed42400ac03c7979d0489c0e2d30e011cb3e3f
Kabita Bhandari <kabitabhandari@Kabitas-MacBook-Pro.local>
2021-06-25 21:55:21 -0500

    chore: created empty function that would return an int

---
+++ MySolutions/src/main/java/com/example/demo/DrawingBook.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class DrawingBook {
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        return 0;
+
+
+    }
+
+    public static void main(String[] args) {
+        int result =  pageCount(16,10);
+        System.out.println("page flip is  : " + result);
+    }
+
+}
