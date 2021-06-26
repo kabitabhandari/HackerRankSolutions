8ebd49d4fdb0fc017a79fa2fb46f16a7d5956ae0
Kabita Bhandari <kabitabhandari@Kabitas-MacBook-Pro.local>
2021-06-25 23:45:31 -0500

    chore: pageCount completed

--- MySolutions/src/main/java/com/example/demo/DrawingBook.java
+++ MySolutions/src/main/java/com/example/demo/DrawingBook.java
@@ -1,16 +1,36 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class DrawingBook {
     public static int pageCount(int n, int p) {
-        // Write your code here
-        return 0;
+        int count =0;
 
+        //  start counting from first page
+        if(p <= n/2 && p != 1){ // if size is less than half then count from first
+            for(int i=2; i<=p; i++){ // loop: 4 if size =18
+                count= count +1;
+                i++;
+            }
+        }
 
+         //  start counting from last page
+        if(p > n/2 && n%2  == 0 && n != p) { // need to flip from back but total pages are even
+                for (int i = n-1; i >= p; i--) {
+                    count++;
+                    i--;
+                }
+            }
+
+        if(p > n/2 && n % 2 !=0 && n != p) { // need to flip from back but total pages are odd
+            for (int i = n-2; i >= p; i--) {
+                count++;
+                i--;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int result =  pageCount(16,10);
+        int result =  pageCount(16,7);
         System.out.println("page flip is  : " + result);
     }
 
