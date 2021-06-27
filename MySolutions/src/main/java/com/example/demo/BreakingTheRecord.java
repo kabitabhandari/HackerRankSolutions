304424a1245c90dec2d6643b76fd72ccd37ce7ec
Kabita Bhandari <kabitabhandari@Kabitas-MacBook-Pro.local>
2021-06-26 19:19:21 -0500

    chore: completed breaking the record

--- MySolutions/src/main/java/com/example/demo/BreakingTheRecord.java
+++ MySolutions/src/main/java/com/example/demo/BreakingTheRecord.java
@@ -1,17 +1,40 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class BreakingTheRecord {
     public static List<Integer> breakingRecords(List<Integer> scores) {
         // Write your code here
+        int min = scores.get(0);
+        int max = scores.get(0);
+        int countMin = 0;
+        int countMax = 0;
+
+        List<Integer> countArrayList = new ArrayList<>();
 
 
-        return scores;
+        for(int i=1; i<scores.size();i++){
+            if(min>scores.get(i)){
+                min = scores.get(i);
+                countMin++;
+            }
+
+            if(max<scores.get(i)){
+                max = scores.get(i);
+                countMax++;
+            }
+
+        }
+        countArrayList.add(countMax);
+        countArrayList.add(countMin);
+
+
+        return countArrayList;
 
     }
     public static void main(String[] args) {
-        List<Integer> a = Arrays.asList(1, 4,4,4,5,3, 5,5);
-        breakingRecords(a);
+        List<Integer> a = Arrays.asList(10,5,20,20,4,5,2,25,1);
+        System.out.println(breakingRecords(a));
     }
 }
