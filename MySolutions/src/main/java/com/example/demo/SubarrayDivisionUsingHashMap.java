9bc2b37c66957c2dbb00174c58ffebd616bdbcde
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-06-29 17:51:35 -0500

    chore: Sales by Match using Hashmap

---
+++ MySolutions/src/main/java/com/example/demo/SubarrayDivisionUsingHashMap.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class SubarrayDivisionUsingHashMap {
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        HashMap<Integer, Integer> myMap = new HashMap<>();
+        int pair = 0;
+        int count = 0;
+        List<Integer> lookupList = new ArrayList<>();
+
+        for (int i = 0; i < ar.size(); i++) {
+            if(!lookupList.contains(ar.get(i))) {
+                for (int j = i; j < ar.size(); j++) {
+                    if (ar.get(i) == ar.get(j)) {
+                        lookupList.add(ar.get(i));
+
+                        count++;
+                    }
+                }
+                myMap.put(ar.get(i), count);
+                count = 0;
+            }
+
+        }
+        //printing hash map
+        System.out.println("myMap is::::" + myMap);
+        //iterating hash map
+        for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+            //System.out.println("map value : " + entry.getValue());
+            if (entry.getValue() % 2 == 0) {
+                pair = pair + entry.getValue() / 2;
+            } else {
+                pair = pair + (entry.getValue() - 1) / 2;
+            }
+        }
+        return pair;
+    }
+
+    public static void main(String[] args) {
+        int n= 7;
+        Integer[] number = {2,2,2,1,1};
+        List<Integer> ar = Arrays.asList(number);
+        System.out.println(sockMerchant(n, ar));
+    }
+}
