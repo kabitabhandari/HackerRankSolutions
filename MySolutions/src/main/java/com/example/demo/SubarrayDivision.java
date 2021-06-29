d656ba6d2868d969d9994068558e438f98fd52c2
Kabita Bhandari <kabitabhandari@Kabitas-MacBook-Pro.local>
2021-06-28 19:48:23 -0500

    chore: SubArrayDivision

---
+++ MySolutions/src/main/java/com/example/demo/SubarrayDivision.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class SubarrayDivision {
+    public static int birthday(List<Integer> s, int d, int m) {
+        int count = 0;
+        int sum =0;
+
+        if(d<m){
+            return count;
+        }
+
+        for(int i =0; i < s.size(); i++){
+
+            for (int j = i; j<m+i && j < s.size(); j++){
+                sum = sum + s.get(j);
+
+            }
+
+            if(sum == d){
+                count ++;
+            }
+            sum =0;
+
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        List<Integer> s = Arrays.asList(1,2,1,3,2);
+        int d = 3;  // total sum
+        int m = 2;  // cells
+        System.out.println(birthday(s,d,m));
+    }
+}
