58bf490dcf5755a75d9c8e229c871d2149da6b13
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2021-07-06 16:43:47 -0500

    chore: Picking Number soln

--- MySolutions/src/main/java/com/example/demo/PickingNumbers.java
+++ MySolutions/src/main/java/com/example/demo/PickingNumbers.java
@@ -1,18 +1,24 @@
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PickingNumbers {
     public static int pickingNumbers(List<Integer> a) {
-        // Write your code here
-        Collections.sort(a);
-        System.out.println(a);
-    return 0;
+        int frequency[] = new int[101];
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < a.size(); i++) {
+            int index=a.get(i);
+            frequency[index]=frequency[index]+1;
+        }
+
+        for (int i = 1; i <= a.size(); i++) {
+            int t = frequency[i] + frequency[i - 1];
+            result = Math.max(result,t);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        Integer[] number = {4,6,5,3,3,1};
+        Integer[] number = {4,6,5,3,3,1,3};
         List<Integer> ar = Arrays.asList(number);
         System.out.println(pickingNumbers(ar));
     }
